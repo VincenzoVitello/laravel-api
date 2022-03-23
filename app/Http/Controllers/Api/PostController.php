28248610811ab5aebf002c0c30be9b3e6e34cf3b
Your Name <you@example.com>
2022-03-23 17:59:48 +0100

    Api popolata di post

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -2,15 +2,19 @@
 
 namespace App\Http\Controllers\Api;
 
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
     public function index(){
-        return response()->json([
-            "nome" => "Vincenzo",
-            "cognome" => "Vitello"
-        ]);
+        // return response()->json([
+        //     "nome" => "Vincenzo",
+        //     "cognome" => "Vitello"
+        // ]);
+        $posts = Post::all();
+        return response()->json($posts);
     }
 }
