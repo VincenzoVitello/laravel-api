e61db5b7bef52ea490c94713b76052835f5f58b6
Your Name <you@example.com>
2022-03-24 17:37:51 +0100

    aggiunto un po' di bootstrap

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,24 +1,24 @@
 <form action="{{route('admin.posts.store')}}" method="POST">
-    @csrf 
-    <label for="tags">Tags</label>
-    @foreach ($tags as $tag)
-      <div>
-        <input type="checkbox" name="tags[]" value="{{$tag->id}}" id="">
-        <label for="">{{$tag->name}}</label>
-      </div>
-    @endforeach
+  @csrf 
+  <label for="tags">Tags</label>
+  @foreach ($tags as $tag)
+    <div>
+      <input type="checkbox" name="tags[]" value="{{$tag->id}}" id="">
+      <label for="">{{$tag->name}}</label>
+    </div>
+  @endforeach
 
 
-    <label for="title">Oggetto del post</label>
-    <input type="text" name="title" id="" placeholder="inserirsci oggetto post">
+  <label for="title">Oggetto del post</label>
+  <input type="text" name="title" id="" placeholder="inserirsci oggetto post">
 
-    <label for="description">Corpo del post</label>
-    <input type="text" name="content" id="" placeholder="cosa vorresti scrivere?">
-    <select name="category_id" id="">
-        <option value="">-- Seleziona categoria --</option>
-        @foreach ($categories as $category)
-           <option value="{{$category->id}}">{{$category->name}}</option>
-        @endforeach
-      </select>
-    <button type="submit" value="Submit">aggiungi</button>
+  <label for="description">Corpo del post</label>
+  <input type="text" name="content" id="" placeholder="cosa vorresti scrivere?">
+  <select name="category_id" id="">
+      <option value="">-- Seleziona categoria --</option>
+      @foreach ($categories as $category)
+         <option value="{{$category->id}}">{{$category->name}}</option>
+      @endforeach
+    </select>
+  <button type="submit" value="Submit">aggiungi</button>
 </form>
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -8,27 +8,52 @@
         <span aria-hidden="true">&times;</span>
         </button>
     </div>
-    <a href="{{route('admin.posts.create')}}"><button>crea nuovo post</button></a>
+    
 
 @endauth
-@foreach($posts as $post)
-    <div class="posts_container">
-        <h1>{{$post->title}}</h1>
-        <h2>categorie: {{$post->category? $post->category->name : 'non appartiene a nessuna categoria'}}</h2>
-        <p>{{$post->content}}</p>
-        <p>tags: </p>
-            @foreach ($post->tags as $tag)
-                {{$tag->name}}
-            @endforeach
-        
-        
-        <a href="{{route('admin.posts.show', $post->id)}}"><button>mostra post</button></a>
-        <form action="{{route("admin.posts.destroy", $post->id)}}" method="POST">
-            @csrf
-            @method("DELETE")
-            <button type="submit" class="" onclick="return confirm('Sei sicuro di voler eliminare questo post?');">Elimina</button>
-        </form>
-    </div>
-@endforeach
+<div class="d-flex justify-content-center flex-column align-items-center">
+    <h1>Lista Posts</h1>
+    <a href="{{ route('admin.posts.create') }}"><button type="button" class="btn btn-warning">Crea Post</button></a>
+</div>
+<table class="table">
+    <thead class="thead-dark">
+        <tr>
+            <th scope="col">id</th>
+            <th scope="col">Titolo</th>
+            <th scope="col">Categoria</th>
+            <th scope="col">Contenuto</th>
+            <th scope="col">Tags</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($posts as $post)
+            <tr>
+                <th scope="row">{{ $post->id }}</th>
+                <td>{{ $post->title }}</td>
+                <td>{{ $post->category ? $post->category->name : '-' }}</td>
+                <td>{{ $post->content }}</td>
+                <td>
+                    @foreach ($post->tags as $tag)
+                        {{ $tag->name }}
+                    @endforeach
+                </td>
+                <td class="d-flex flex-column align-items-center">
+                    <a href="{{ route('admin.posts.show', $post->id) }}">
+                        <button type="button" class="btn btn-primary">Mostra</button>
+                    </a>
+                    <a href="{{ route('admin.posts.edit', $post->id) }}">
+                        <button type="button"
+                        class="btn btn-warning">Modifica</button>
+                    </a>
+                    <form action="{{ route('admin.posts.destroy', $post->id) }}" method="POST">
+                        @csrf
+                        @method("DELETE")
+                        <button type="submit" class="btn btn-danger">Cancella</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
 
 @endsection
